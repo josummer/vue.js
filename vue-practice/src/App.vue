6ae8c977f7f3bc1f6c2a93148340253110c50074
josummer <nm354696@gmail.com>
2022-02-01 23:10:26 +0900

    eleventh commit_formHandling

--- vue-practice/src/App.vue
+++ vue-practice/src/App.vue
@@ -1,21 +1,62 @@
+<!--폼 
+    : 어떤 입력 양식을 받고, 받은 내용을 서버 등으로 보내 저장하고, 
+      일정 형식에 맞는 양식 -->
 <template>
   <div>
-    <h1>Hello {{ name }}!</h1>
-    <button v-on:click.middle="changeName">change name</button>
-    <!--<modifier> 클릭 left는 default / 
-        middle : 가운데 휠 클릭, right : 오른쪽 휠 클릭-->
-    <button
-      v-on:mouseover="name = 'Code Scalper'"
-      v-on:mouseleave="name = 'Scalper'"
-    >
-      change name
-    </button>
-    <a v-on:click.prevent="movePage" href="https://naver.com">naver로 이동</a>
-    <h2>{{ number }}</h2>
-    <button v-on:click="increment($event, 1)">숫자 1 증가</button>
-    <button v-on:click="decrement(1)">숫자 1 감소</button>
-    <button v-on:click="increment(5)">숫자 5 증가</button>
-    <button v-on:click="decrement(5)">숫자 5 감소</button>
+    {{ user }}
+    <h1>{{ user.name }}</h1>
+    <hr />
+    <form>
+      <div>
+        <!--IME (Input Method Editor)
+        : 자판에 있는 글자보다 수가 많은 문자를 조합하여 입력해주는 시스템
+          ==> setValue와 같은 메서드 사용해서 해결 가능-->
+        <label for="name">이름</label>
+        <input type="text" id="name" v-model="user.name" @input="setValue" />
+      </div>
+      <div>
+        <label for="age">나이</label>
+        <input type="number" id="age" v-model="user.age" />
+      </div>
+      <div>
+        <label for="city">사는 곳</label>
+        <select id="city" v-model="user.city">
+          <option value="seoul">서울</option>
+          <option value="daejeon">대전</option>
+          <option value="daegu">대구</option>
+          <option value="busan">부산</option>
+          <option value="gwangju">광주</option>
+        </select>
+      </div>
+      <div>
+        <label for="favorite-food">좋아하는 음식</label>
+        <select id="favorite-food" multiple v-model="user.favorite">
+          <!--multiple 키 : ctrl을 누르고 클릭을 하면 여러 개를 한번에 클릭할 수 있음-->
+          <option
+            v-for="option in foodOptions"
+            :value="option.code"
+            :key="option.code"
+          >
+            {{ option.label }}
+          </option>
+        </select>
+      </div>
+      <div>
+        <label for="job">직업</label>
+        프로그래머<input
+          type="checkbox"
+          value="programmer"
+          v-model="user.job"
+        />
+        가수<input type="checkbox" value="singer" v-model="user.job" />
+        교사<input type="checkbox" value="teacher" v-model="user.job" />
+      </div>
+      <div>
+        <label for="gender">성별</label>
+        <input type="radio" value="male" v-model="user.gender" />
+        <input type="radio" value="female" v-model="user.gender" />
+      </div>
+    </form>
   </div>
 </template>
 
@@ -24,38 +65,36 @@ export default {
   name: "App",
   data() {
     return {
-      name: "Scalper",
-      number: 0,
+      foodOptions: [
+        { label: "짜장면", code: "JJ" },
+        { label: "짬뽕", code: "JB" },
+        { label: "탕수육", code: "TS" },
+      ],
+      user: {
+        name: "",
+        age: 0,
+        city: "seoul",
+        favorite: [],
+        job: [],
+      },
     };
   },
   methods: {
-    changeName() {
-      this.name = "Code Scalper";
-    },
-    movePage() {
-      //e.preventDefault(); // 기본적으로 탑제되있는 페이지 이동 기능을 막아줌
-      // parameter로 e 줘야 함 (지금은 위에 쓰느냐 생략)
-      const check = confirm("페이지를 이동하시겠습니까?");
-      if (check) {
-        console.log("page 이동");
-      } else {
-        console.log("page 이동안함");
-      }
-    },
-    increment(e, num) {
-      console.log(e);
-      this.number += num;
-    },
-    decrement(num) {
-      this.number -= num;
+    setValue(e) {
+      console.log(e.target.value);
+      this.user.name = e.target.value;
     },
   },
 };
 </script>
 
 <style>
-a {
-  font-size: 24px;
-  display: block;
+label {
+  font-size: 22px;
+  font-weight: bold;
+  margin-right: 1rem;
+}
+div {
+  margin-bottom: 1rem;
 }
 </style>
